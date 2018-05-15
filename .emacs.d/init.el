46cbc65fb8d7f8a9a1ded48d7bf313b4c2053d46
kreedz <kreedz@users.noreply.github.com>
2018-05-15 13:51:51 +0500

    emacs: ivy hotkeys

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,10 +45,10 @@
 (setq package-list
       '(add-node-modules-path
 	company
+	counsel
 	evil
 	evil-leader
 	git-gutter
-	ivy
 	js2-mode
 	magit
 	solarized-theme
@@ -231,3 +231,12 @@
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x C-g") 'counsel-git)
+(global-set-key (kbd "C-x C-r") 'counsel-recentf)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> b") 'counsel-descbinds)
