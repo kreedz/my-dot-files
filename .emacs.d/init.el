4f8c591821ab88419099552a320c55913ca8378e
kreedz <kreedz@users.noreply.github.com>
2018-06-09 18:54:45 +0500

    emacs: treemacs init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,6 +59,9 @@
         smartparens
         solarized-theme
         tide
+        treemacs
+        treemacs-evil
+        treemacs-projectile
         web-mode))
 
 ;; list the repositories containing them
@@ -148,16 +151,16 @@
 ;; change mode-line color by evil state
 (require 'cl)
 (lexical-let ((default-color (cons (face-background 'mode-line)
-                                  (face-foreground 'mode-line))))
- (add-hook 'post-command-hook
-   (lambda ()
-     (let ((color (cond ((minibufferp) default-color)
-                        ((evil-insert-state-p) '("#a6a6a6" . "#ffffff")) ; grey
-                        ((evil-emacs-state-p)  '("#444488" . "#ffffff")) ; purple
-                        ((buffer-modified-p)   '("#006fa0" . "#ffffff")) ; blue
-                        (t default-color))))
-          (set-face-background 'mode-line (car color))
-          (set-face-foreground 'mode-line (cdr color))))))
+                                   (face-foreground 'mode-line))))
+(add-hook 'post-command-hook
+  (lambda ()
+    (let ((color (cond ((minibufferp) default-color)
+                       ((evil-insert-state-p) '("#a6a6a6" . "#ffffff")) ; grey
+                       ((evil-emacs-state-p)  '("#444488" . "#ffffff")) ; purple
+                       ((buffer-modified-p)   '("#006fa0" . "#ffffff")) ; blue
+                       (t default-color))))
+      (set-face-background 'mode-line (car color))
+      (set-face-foreground 'mode-line (cdr color))))))
 
 
 ;; add-node-modules-path
@@ -268,6 +271,7 @@
 
 ;; smartparens
 (require 'smartparens-config)
+(show-smartparens-global-mode t)
 ;; Always start smartparens mode in js-mode.
 (add-hook 'prog-mode-hook #'smartparens-mode)
 
@@ -276,3 +280,45 @@
       '(typescript-mode web-mode)
     (sp-local-pair "{" nil :post-handlers '(:add ("||\n[i]" "RET")))
     (sp-local-pair "(" nil :post-handlers '(:add ("||\n[i]" "RET")))))
+
+
+;; treemacs
+(global-set-key (kbd "M-0") 'treemacs-select-window)
+(global-set-key (kbd "C-x t 1") 'treemacs-delete-other-windows)
+(global-set-key (kbd "C-x t t") 'treemacs)
+(global-set-key (kbd "C-x t B") 'treemacs-bookmark)
+(global-set-key (kbd "C-x t C-t") 'treemacs-find-file)
+(global-set-key (kbd "C-x t M-t") 'treemacs-find-tag)
+(with-eval-after-load 'treemacs
+  (message "hello")
+  (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+        treemacs-file-event-delay           5000
+        treemacs-follow-after-init          t
+        treemacs-follow-recenter-distance   0.1
+        treemacs-goto-tag-strategy          'refetch-index
+        treemacs-indentation                2
+        treemacs-indentation-string         " "
+        treemacs-is-never-other-window      nil
+        treemacs-no-png-images              nil
+        treemacs-project-follow-cleanup     nil
+        treemacs-recenter-after-file-follow nil
+        treemacs-recenter-after-tag-follow  nil
+        treemacs-show-hidden-files          t
+        treemacs-silent-filewatch           nil
+        treemacs-silent-refresh             nil
+        treemacs-sorting                    'alphabetic-desc
+        treemacs-tag-follow-cleanup         t
+        treemacs-tag-follow-delay           1.5
+        treemacs-width                      35)
+
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (pcase (cons (not (null (executable-find "git")))
+                (not (null (executable-find "python3"))))
+    (`(t . t)
+     (treemacs-git-mode 'extended))
+    (`(t . _)
+     (treemacs-git-mode 'simple)))
+
+  (require 'treemacs-evil)
+  (require 'treemacs-projectile))
