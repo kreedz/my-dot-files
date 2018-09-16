ad0787592fa98b0074f0078839952119cd489f2e
kreedz <kreedz@users.noreply.github.com>
2018-09-16 19:23:45 +0500

    bin: set dvorak layout, set caps as esc in case if tap it alone and set caps as ctrl if tap if press caps with another key

--- .bin/startup.sh
+++ .bin/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-setxkbmap us,ru -option grp:ctrl_shift_toggle
+#setxkbmap us,ru -option grp:ctrl_shift_toggle
+setxkbmap us,ru -option -variant dvp, 
+xcape -e 'Control_L=Escape'
 # setxkbmap -option "ctrl:swapcaps"
-xmodmap -e "keycode 69 = Tab"
+# xmodmap -e "keycode 69 = Tab"
 xbindkeys
 pgrep -u $USER -x nm-applet || (nm-applet &)
 pgrep -u $USER -x xflux || (xflux -l 54.44N -g 55.58E &)
