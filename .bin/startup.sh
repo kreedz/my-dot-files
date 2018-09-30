c326ebc8b8957f09236e523d742f2dcc8d3098f3
kreedz <kreedz@users.noreply.github.com>
2018-09-30 23:31:29 +0500

    key bindings: left shift as backspace when pressed alone left shift plus space as enter

--- .bin/startup.sh
+++ .bin/startup.sh
@@ -3,6 +3,7 @@ setxkbmap us,ru -option -variant dvp,
 setxkbmap -option ctrl:nocaps
 setxkbmap -option grp:alt_shift_toggle
 xcape -e 'Control_L=Escape'
+xcape -e 'Shift_L=BackSpace'
 xbindkeys
 pgrep -u $USER -x nm-applet || (nm-applet &)
 pgrep -u $USER -x xflux || (xflux -l 54.44N -g 55.58E &)
