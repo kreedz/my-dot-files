dc57589a1b7d71e256efe77a8faac7673f318b14
kreedz <kreedz@users.noreply.github.com>
2018-09-30 20:52:58 +0500

    bin: remove caps, add toggle

--- .bin/startup.sh
+++ .bin/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#setxkbmap us,ru -option grp:ctrl_shift_toggle
 setxkbmap us,ru -option -variant dvp, 
+setxkbmap -option ctrl:nocaps
+setxkbmap -option grp:alt_shift_toggle
 xcape -e 'Control_L=Escape'
-# setxkbmap -option "ctrl:swapcaps"
-# xmodmap -e "keycode 69 = Tab"
 xbindkeys
 pgrep -u $USER -x nm-applet || (nm-applet &)
 pgrep -u $USER -x xflux || (xflux -l 54.44N -g 55.58E &)
